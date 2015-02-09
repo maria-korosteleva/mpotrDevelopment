23db76e8a62000593eac276f4499266938e0e9ab
Maria Korosteleva <mariak@seclab.cs.msu.su>
2015-02-09 15:37:37 +0400

    libraries

---
+++ libraries/dbus-glib-0.102/config.h
@@ -0,0 +1,85 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Enable bash completion */
+#define DBUS_BASH_COMPLETION 1
+
+/* Build test code */
+/* #undef DBUS_BUILD_TESTS */
+
+/* Defined to the gcc version if gcov is enabled, to force a rebuild due to
+   config.h changing */
+/* #undef DBUS_GCOV_ENABLED */
+
+/* Disable GLib assertion macros */
+#define G_DISABLE_ASSERT 1
+
+/* Disable GLib public API sanity checking */
+/* #undef G_DISABLE_CHECKS */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <expat.h> header file. */
+#define HAVE_EXPAT_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Have socklen_t type */
+#define HAVE_SOCKLEN_T 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "dbus-glib"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=dbus&component=GLib"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "dbus-glib"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "dbus-glib 0.102"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "dbus-glib"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.102"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.102"
